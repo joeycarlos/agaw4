cf605d5e85f92ff64bf2fd2e9ceccf89e2517554
joeycarlos <jclaguio@gmail.com>
2019-11-13 14:38:06 -0800

    Add arrow prefab and ability to shoot

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private Vector3 moveVector;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(moveVector * Time.deltaTime);
+    }
+
+    public void InitArrow(Vector3 newVector, float projectileSpeed) {
+        // calculate the movement vector of fire projectile per update frame
+        moveVector = newVector;
+        moveVector.z = 0;
+        moveVector = Vector3.Normalize(newVector);
+        moveVector *= projectileSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject arrow;
+
+    public float fireRate = 1.0f;
+    public float arrowSpeed = 10.0f;
+
+    private Vector3 mousePos;
+    private float timeSinceFire;
+
+    void Update() {
+        timeSinceFire -= Time.deltaTime;
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKey("mouse 0")) {
+            if (timeSinceFire <= 0)
+                ShootArrow();
+        }
+    }
+
+    private void ShootArrow() {
+        Vector3 direction;
+
+        direction = mousePos - transform.position;
+        direction.z = 0;
+
+        GameObject iArrow;
+        iArrow = Instantiate(arrow, transform.position, Quaternion.identity);
+        iArrow.GetComponent<Arrow>().InitArrow(direction, arrowSpeed);
+
+        timeSinceFire = fireRate;
+    }
+
+
+}
